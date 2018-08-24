71ed4ce9d2faab70539b80c23e04d00a890893bd
song <song@songs-MacBook-Air.local>
2018-08-24 15:19:11 +0800

    baiduFaceApi
    baiduFaceApi

---
+++ src/Face/FaceApi/Api/Lib/AipSignOption.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: song
+ * Date: 2018/8/24
+ * Time: 上午11:35
+ */
+namespace FaceApi\Lib;
+
+
+class AipSignOption
+{
+    const EXPIRATION_IN_SECONDS = 'expirationInSeconds';
+
+    const HEADERS_TO_SIGN = 'headersToSign';
+
+    const TIMESTAMP = 'timestamp';
+
+    const DEFAULT_EXPIRATION_IN_SECONDS = 1800;
+
+    const MIN_EXPIRATION_IN_SECONDS = 300;
+
+    const MAX_EXPIRATION_IN_SECONDS = 129600;
+}
